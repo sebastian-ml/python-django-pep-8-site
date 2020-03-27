b6cec18ba047eb1d24b537ce2de1c3e3d10e2483
sebastian-ml <sebastian2549@wp.pl>
2020-03-27 19:24:23 +0100

    Check the uniqueness of an email during registration, auto create profile

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,23 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
+    def clean_email(self):
+        """Get an e-mail and check if it already exists in db."""
+
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email):
+            raise ValidationError("Podany adres e-mail jest już wykorzystywany")
+        return email
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = [
+            'username', 'email',
+            'password1', 'password2',
+        ]
--- users/models.py
+++ users/models.py
@@ -38,4 +38,4 @@ class Profile(models.Model):
                            )
 
     def __str__(self):
-        return self.user.username
+        return f'{self.user.username} profile'
