484020783d5e382b43d28765326e19cc187ecbdb
sebastian-ml <sebastian2549@wp.pl>
2020-04-13 16:14:38 +0200

    Add docstrings in 'main' views. Describe the behavior of each view.

--- main/views.py
+++ main/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 
 
 def home(request):
+    """Display main page."""
     return render(request, 'main/main.html')
 
 
 def contact(request):
+    """Display contact page. Set the heading also."""
     context = {'heading': 'Contact'}
     return render(request, 'main/contact.html', context)
 
 
 def about(request):
+    """Display about page. Set the heading also."""
     context = {'heading': 'About'}
     return render(request, 'main/about.html', context)
