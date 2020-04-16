3643d9a376b2b7e27b70d11789c7e73d541287f9
sebastian-ml <sebastian2549@wp.pl>
2020-04-16 23:39:11 +0200

    Make 'base' template more clear by changing indentation. Add 'featured subpages' Create featured subpages using class inheritance from featured-subpage1

--- forum/urls.py
+++ forum/urls.py
@@ -3,15 +3,28 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.HomeTemplateView.as_view(), name='forum-main'),
+    path('',
+         views.HomeTemplateView.as_view(),
+         name='forum-main'),
     path('community/',
          views.CommunityTemplateView.as_view(),
-         name='forum-community'
-         ),
+         name='forum-community'),
     path('featured/',
          views.FeaturedTemplateView.as_view(),
          name='forum-featured'),
     path('featured/subpage1/',
          views.FeaturedSubpage1TemplateView.as_view(),
          name='forum-featured-subpage1'),
+    path('featured/subpage2/',
+         views.FeaturedSubpage2TemplateView.as_view(),
+         name='forum-featured-subpage2'),
+    path('featured/subpage3/',
+         views.FeaturedSubpage3TemplateView.as_view(),
+         name='forum-featured-subpage3'),
+    path('featured/subpage4/',
+         views.FeaturedSubpage4TemplateView.as_view(),
+         name='forum-featured-subpage4'),
+    path('featured/subpage5/',
+         views.FeaturedSubpage5TemplateView.as_view(),
+         name='forum-featured-subpage5'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -18,7 +18,7 @@ class CommunityTemplateView(TemplateView):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
         context['heading'] = 'Społeczność'
-        context['first_previous_page'] = 'Forum'
+        context['first_subpage'] = 'Forum'
         return context
 
 
@@ -29,7 +29,7 @@ class FeaturedTemplateView(TemplateView):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
         context['heading'] = 'Featured'
-        context['first_previous_page'] = 'Forum'
+        context['first_subpage'] = 'Forum'
         return context
 
 
@@ -41,6 +41,44 @@ class FeaturedSubpage1TemplateView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['heading'] = 'Featured'
         context['subheading'] = 'Category1'
-        context['first_previous_page'] = 'Featured'
-        context['second_previous_page'] = 'Forum'
+        context['first_subpage'] = 'Forum'
+        context['second_subpage'] = 'Featured'
         return context
+
+
+class FeaturedSubpage2TemplateView(FeaturedSubpage1TemplateView):
+    template_name = 'forum/featured-subpage2.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['subheading'] = 'Category2'
+        return context
+
+
+class FeaturedSubpage3TemplateView(FeaturedSubpage1TemplateView):
+    template_name = 'forum/featured-subpage3.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['subheading'] = 'Category3'
+        return context
+
+
+class FeaturedSubpage4TemplateView(FeaturedSubpage1TemplateView):
+    template_name = 'forum/featured-subpage4.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['subheading'] = 'Category4'
+        return context
+
+
+class FeaturedSubpage5TemplateView(FeaturedSubpage1TemplateView):
+    template_name = 'forum/featured-subpage5.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['subheading'] = 'Category5'
+        return context
+
+
