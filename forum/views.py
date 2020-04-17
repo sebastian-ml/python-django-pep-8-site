7347400db6519e4aae70382b62922e9a011c0349
sebastian-ml <sebastian2549@wp.pl>
2020-04-17 19:03:38 +0200

    Simplify views in 'forum' by creating one class for subpages Create one CBV - same appereance but different posts

--- forum/views.py
+++ forum/views.py
@@ -28,52 +28,19 @@ def forum_featured(request):
     return render(request, 'forum/featured.html', context)
 
 
-class FeaturedSubpage1TemplateView(TemplateView):
-    template_name = 'forum/featured-subpage1.html'
+class FeaturedSubpageView(TemplateView):
+    subheading = None
 
     def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
+        # Add custom context keys
+        # First 3 context keys are the same for each featured subpage
         context['heading'] = 'Featured'
-        context['subheading'] = 'Category1'
         context['first_subpage'] = 'Forum'
         context['second_subpage'] = 'Featured'
-        return context
+        # Subheading is different for each featured subpage
+        context['subheading'] = self.subheading
 
-
-class FeaturedSubpage2TemplateView(FeaturedSubpage1TemplateView):
-    template_name = 'forum/featured-subpage2.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['subheading'] = 'Category2'
-        return context
-
-
-class FeaturedSubpage3TemplateView(FeaturedSubpage1TemplateView):
-    template_name = 'forum/featured-subpage3.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['subheading'] = 'Category3'
-        return context
-
-
-class FeaturedSubpage4TemplateView(FeaturedSubpage1TemplateView):
-    template_name = 'forum/featured-subpage4.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['subheading'] = 'Category4'
-        return context
-
-
-class FeaturedSubpage5TemplateView(FeaturedSubpage1TemplateView):
-    template_name = 'forum/featured-subpage5.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['subheading'] = 'Category5'
         return context
 
 
@@ -81,5 +48,6 @@ class PostCreateView(CreateView, LoginRequiredMixin):
     model = Post
     template_name = 'forum/post_create.html'
     fields = ['title', 'content']
+
 
 
