6afdfd8b506670c35a4bd77a79076ac61000f389
sebastian-ml <sebastian2549@wp.pl>
2022-08-18 22:25:46 +0200

    Update contact page

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fb!03vh#)@1_8bq67+kuyv2gn&b5c0rmqa_h!wpxmy+9odgh8y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
